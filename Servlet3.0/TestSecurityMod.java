fc5d46609dbd5a85339e4600f639c5aefdbe2c71
sharp <kigyouu@yahoo.co.jp>
2014-12-04 11:50:48 +0900

    Create TestSecurityMod.java

---
+++ Servlet3.0/TestSecurityMod.java
@@ -0,0 +1,16 @@
+//It is not able to respond other than GET method.
+@ServletSecurity(value = @HttpConstraint(EmptyRoleSemantic.DENY), httpMethodConstraints = @HttpMethodConstraint("GET"))
+public class TestSecurityMod extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException {
+    doSomething();
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException {
+    doSomething();
+  }
+}
